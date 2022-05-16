022ad50759c8d555ec8d850fa0b1a7d273a6d99d
Azim3triko <azim3triko@protonmail.com>
2022-05-16 02:50:47 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,48 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let generateRandomNumber = () => {
+    let number = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let indexNumber = Math.floor(Math.random() * number.length);
+    console.log(indexNumber);
+    return number[indexNumber];
+  };
+
+  let randomPinta = generateRandomPinta();
+  document.querySelector(".top-suit").textContent = randomPinta;
+  document.querySelector(".bottom-suit").textContent = randomPinta;
+  document.querySelector(".bottom-suit").classList.add("boca-arriba");
+
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+
+  function generateRandomPinta() {
+    let Pinta = ["♦", "♥", "♣", "♠"];
+    let indexPinta = Math.floor(Math.random() * Pinta.length);
+    return Pinta[indexPinta];
+  }
+
+  // identificar diamante y corazon
+  if (randomPinta == "♦" || randomPinta == "♥") {
+    document.querySelector(".top-suit").style.color = "red";
+    document.querySelector(".bottom-suit").style.color = "red";
+    document.querySelector(".number").style.color = "red";
+  }
+
+  // si es uno de ellos, transformar esa seleccción y su número a rojo
 };
